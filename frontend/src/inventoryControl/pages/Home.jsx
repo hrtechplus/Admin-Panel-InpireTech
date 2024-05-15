74870ee9aca826ecceef72bce940a51c998c997d
HasinduOnline <40927446+hrtechplus@users.noreply.github.com>
2024-05-15 14:38:46 +0530

    uodres some routes

--- frontend/src/inventoryControl/pages/Home.jsx
+++ frontend/src/inventoryControl/pages/Home.jsx
@@ -1,55 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Input } from '@chakra-ui/react'
+import React, { useEffect, useState } from "react";
+import { Container, Input } from "@chakra-ui/react";
 
 // Components
-import ItemDetailsDisplay from '../Components/ItemDetailsDisplay'
-import SlideBar from '../Components/SlideBar'
-
+import ItemDetailsDisplay from "../Components/ItemDetailsDisplay";
+import SlideBar from "../Components/SlideBar";
 
 function Home() {
   // States
-  const [items, setItems] = useState(null)
-  const [query, setQuery] = useState('')
+  const [items, setItems] = useState(null);
+  const [query, setQuery] = useState("");
 
   // fetch items data from server
   useEffect(() => {
     const fetchItems = async () => {
-      const response = await fetch('/inventoryPanel')
-      const json = await response.json()
+      const response = await fetch("http://localhost:5001/inventoryPanel");
+      const json = await response.json();
 
-      if(response.ok) {
-        setItems(json)
+      if (response.ok) {
+        setItems(json);
       }
-    }
-    fetchItems()
-  }, [])
+    };
+    fetchItems();
+  }, []);
 
   return (
     <>
       <SlideBar />
       <Container>
         <Input
-          placeholder='Search Items' 
-          marginTop='24px' 
-          marginBottom='36px'
+          placeholder="Search Items"
+          marginTop="24px"
+          marginBottom="36px"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
-        
+
         <div className="itemsDetailsSection">
-          {items && items.filter((item) => {
-            if(query === '') {
-              return item
-            }else if(item.itemName.toLowerCase().includes(query.toLowerCase())) {
-              return item
-            }
-          }).map((item) => (
-            <ItemDetailsDisplay key={item.itemID} item={item} /> //ItemDetailsDisplay component
-          ))}
+          {items &&
+            items
+              .filter((item) => {
+                if (query === "") {
+                  return item;
+                } else if (
+                  item.itemName.toLowerCase().includes(query.toLowerCase())
+                ) {
+                  return item;
+                }
+              })
+              .map((item) => (
+                <ItemDetailsDisplay key={item.itemID} item={item} /> //ItemDetailsDisplay component
+              ))}
         </div>
       </Container>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/inventoryControl/pages/Notification.jsx
+++ frontend/src/inventoryControl/pages/Notification.jsx
@@ -1,40 +1,45 @@
-import { Container, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import { Container, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
 // Components
-import ItemNotification from '../Components/ItemNotification'
-import SlideBar from '../Components/SlideBar'
+import ItemNotification from "../Components/ItemNotification";
+import SlideBar from "../Components/SlideBar";
 
 function Notification() {
-    // States
-    const [items, setItems] = useState(null)
+  // States
+  const [items, setItems] = useState(null);
 
-    // fetch items data from server
-    useEffect(() => {
-        const fetchItems = async () => {
-            const response = await fetch('/inventoryPanel')
-            const json = await response.json()
+  // fetch items data from server
+  useEffect(() => {
+    const fetchItems = async () => {
+      const response = await fetch("http://localhost:5001/inventoryPanel");
+      const json = await response.json();
 
-            if (response.ok) {
-                setItems(json)
-            }
-        }
-        fetchItems()
-    }, [])
+      if (response.ok) {
+        setItems(json);
+      }
+    };
+    fetchItems();
+  }, []);
 
-    return (
-        <>
-            <SlideBar />
-            <Container paddingTop='60px'>
-                <Text fontSize='2xl' paddingBottom='60px'>These Items Reached To Stock Limit Value</Text>
-                <div className="itemsNotificationSection">
-                    {items && items.filter((item) => item.stockCount <= 3).map((item) => (
-                        <ItemNotification key={item.itemID} item={item} />
-                    ))}
-                </div>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <SlideBar />
+      <Container paddingTop="60px">
+        <Text fontSize="2xl" paddingBottom="60px">
+          These Items Reached To Stock Limit Value
+        </Text>
+        <div className="itemsNotificationSection">
+          {items &&
+            items
+              .filter((item) => item.stockCount <= 3)
+              .map((item) => (
+                <ItemNotification key={item.itemID} item={item} />
+              ))}
+        </div>
+      </Container>
+    </>
+  );
 }
 
-export default Notification
+export default Notification;
